c0a739e4f82f390934777fec5345a96afa69efe8
UrielCuriel <urielcuriel@outlook.com>
2024-11-14 02:03:57 -0600

    feat: improve CSP handling and reporting
    Enhanced Content Security Policy by allowing 'unsafe-inline' for both scripts and styles, which could improve compatibility with certain libraries while maintaining security. Added support for handling CSP violation reports by creating a new API route that logs and allows storing reports for further analysis. Modified the report-uri path to point to the new API endpoint, ensuring all violations are captured consistently.

--- next.config.ts
+++ next.config.ts
@@ -49,15 +49,16 @@ const nextConfig: NextConfig = {
             key: 'Content-Security-Policy',
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-eval' https://kit.fontawesome.com https://ka-p.fontawesome.com';
-              style-src 'self' 'sha256-zlqnbDt84zf1iSefLU/ImC54isoprH/MRiVZGskwexk=';
+              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://kit.fontawesome.com https://ka-p.fontawesome.com;
+              style-src 'self' 'unsafe-inline';
+              font-src 'self' https://ka-p.fontawesome.com;
               img-src 'self' blob: data:;
-              font-src 'self';
               connect-src 'self' ${process.env.NEXT_PUBLIC_API_URL} https://ka-p.fontawesome.com;
-              report-uri /csp-violation-report-endpoint;
+              report-uri /api/csp-violation-report;
               upgrade-insecure-requests;
             `.replace(/\s{2,}/g, ' ').trim(),
           }
+
 
         ],
       },
---
+++ src/app/api/csp-violation-report/route.ts
@@ -0,0 +1,25 @@
+
+export async function POST(
+  req: Request
+) {
+  try {
+    // Leer el cuerpo de la solicitud que contiene el reporte CSP
+    const body = await req.json();
+    const report = body['csp-report'];
+
+    console.log('CSP Violation Report:', report);
+
+    // Aquí podrías almacenar el reporte en una base de datos para análisis posterior
+    // await saveReportToDatabase(report);
+
+    return new Response(null, {
+      status: 204, // No Content
+    });
+  } catch (error) {
+    console.error('Error processing CSP report:', error);
+    return new Response(null, {
+      status: 500, // Internal Server Error
+    });
+  }
+}
+
